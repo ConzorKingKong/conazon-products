55b7bb9727cb5b8f3872589870cb512f5ec9bbb1
connor <connor@connors-MacBook-Pro.local>
2024-07-02 23:37:03 -0500

    Add more products to initial server state.

--- init.sql
+++ init.sql
@@ -15,3 +15,7 @@ insert into products.products (name, description, main_image, category, price, q
 values ('How to build an ecommerce store', 'The best book ever', 'https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg', 'ebook', 19.99, 100, 'connor peshek');
 insert into products.products (name, description, main_image, category, price, quantity, author)
 values ('My resume', 'Actually the best book ever', 'https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg', 'ebook', 99.99, 100, 'connor peshek');
+insert into products.products (name, description, main_image, category, price, quantity, author)
+values ('Necronomicon', 'Its cool. I said the words. I did!', 'https://images-cdn.ubuy.co.id/65536d308301b1521c28f473-necronomicon-ex-mortis-illustrated.jpg', 'ebook', 6.66, 666, 'Ruby');
+insert into products.products (name, description, main_image, category, price, quantity, author)
+values ('book 4 - we have gone too far', 'This book should never have been written', 'https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg', 'ebook', 99.99, 100, 'connor peshek');
